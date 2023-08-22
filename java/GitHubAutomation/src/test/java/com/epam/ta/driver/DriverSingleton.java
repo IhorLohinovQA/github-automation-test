5bc89328da9826d27132a5a88200e2b80723b023
Ihor Lohinov <igor.loginov.1997@gmail.com>
2023-08-22 08:54:47 +0300

    changed pom file

--- java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -27,7 +27,7 @@ public class DriverSingleton {
                     break;
                 }
                 default: {
-                    WebDriverManager.chromedriver().driverVersion("116.0.5845.96").setup();
+                    WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
                 }
