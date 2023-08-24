1e14ae74c810b1d2044ce2a6ab54419d8b1be851
IhorLohinovQA <igor.loginov.1997@gmail.com>
2023-08-24 17:30:13 +0300

    added EdgeDriver

--- java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ java/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package com.epam.ta.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
@@ -25,6 +26,12 @@ public class DriverSingleton {
                     //WebDriverManager.safaridriver().setup();
                     System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
                     driver = new SafariDriver();
+                    break;
+                }
+                case "edge": {
+                    //WebDriverManager.edgedriver().setup();
+                    System.setProperty("webdriver.edge.driver", "/usr/local/bin/msedgedriver");
+                    driver = new EdgeDriver();
                     break;
                 }
                 default: {
